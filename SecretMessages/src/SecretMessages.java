0bc387a2c44ed00c00d60579a03bcef1f0b4b96c
John Flynn <40461634+Fullsailer@users.noreply.github.com>
2021-02-01 13:23:41 -0500

    Input and Output Built.
    Built input and out put scanners, which works nicely.

--- SecretMessages/src/SecretMessages.java
+++ SecretMessages/src/SecretMessages.java
@@ -6,7 +6,11 @@ public class SecretMessages {
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Enter a message to encode or decode:");
 		String message = scan.nextLine();
-				
+		String output = "";
+		for ( int x = message.length()-1; x >= 0; x--) {
+			output += message.charAt(x);
+		}
+		System.out.println(output);
 
 	}
 
