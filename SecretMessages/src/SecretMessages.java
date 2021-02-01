b5f4cf4f19392608a347f3cc37a87eb026a29452
John Flynn <40461634+Fullsailer@users.noreply.github.com>
2021-02-01 14:41:52 -0500

    Added Key input update.
    Added a second key input for user to select key. Cod crashed on first run through, but ran fine on second attempt, ghost bugs?

--- SecretMessages/src/SecretMessages.java
+++ SecretMessages/src/SecretMessages.java
@@ -7,10 +7,33 @@ public class SecretMessages {
 		System.out.println("Enter a message to encode or decode:");
 		String message = scan.nextLine();
 		String output = "";
-		for ( int x = message.length()-1; x >= 0; x--) {
-			output += message.charAt(x);
+		System.out.println("Enter a secret key (-25 to 25):");
+		int keyVal = Integer.parseInt(scan.nextLine());
+		char key = (char) keyVal;
+		for (int x = 0; x < message.length(); x++ ) {
+			char input = message.charAt(x);
+			if (input >= 'a' && input <= 'z')
+			{
+				input += key;
+				if (input > 'z')
+					input -=26;
+				if (input < 'a')
+					input += 26;
+			}
+			else if (input >= '0' && input <= '9')
+			{
+				input += (keyVal % 10);
+				if (input > '9')
+					input -= 10;
+				if (input < '0')
+					input += 10;
+			}
+		
+			output += input;
+			// output += (char)(message.charAt(x) + key); old code from version 1
 		}
 		System.out.println(output);
+		scan.close();
 
 	}
 
