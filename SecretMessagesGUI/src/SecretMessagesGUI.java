e607793ab30f7b6817a95d4dd7abb2a86ce7b044
John Flynn <40461634+Fullsailer@users.noreply.github.com>
2021-02-03 16:28:04 -0500

    Coded Message App GUI
    Created the encode Method, Event Handler for Ecode/Decode Button, added Try/Catch block for bad inputs, built Main Methof for running App.

--- SecretMessagesGUI/src/SecretMessagesGUI.java
+++ SecretMessagesGUI/src/SecretMessagesGUI.java
@@ -4,10 +4,48 @@ import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.JLabel;
 import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class SecretMessagesGUI extends JFrame implements SwingConstants {
 	private JTextArea txtIn;
 	private JTextField txtKey;
+	private JTextArea txtOut;
+	public String encode( String message, int keyVal ) {
+		String output = "";
+		char key = (char) keyVal;
+		for (int x = 0; x < message.length(); x++ ) {
+			char input = message.charAt(x);
+			if (input >= 'a' && input <= 'z')
+			{
+				input += key;
+				if (input > 'z')
+					input -=26;
+				if (input < 'a')
+					input += 26;
+			}
+			else if (input >= 'a' && input <= 'z')
+            { 
+                input += key;
+                if (input > 'z')
+                    input -= 26;
+                if (input < 'a')
+                    input += 26;
+            }
+			else if (input >= '0' && input <= '9')
+			{
+			
+				input += (keyVal % 10);
+				if (input > '9')
+					input -= 10;
+				if (input < '0')
+					input += 10;
+			}
+		
+			output += input;
+		}
+		return output;
+	}
 	public SecretMessagesGUI() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		getContentPane().setLayout(null);
@@ -16,7 +54,7 @@ public class SecretMessagesGUI extends JFrame implements SwingConstants {
 		txtIn.setBounds(6, 6, 543, 182);
 		getContentPane().add(txtIn);
 		
-		JTextArea txtOut = new JTextArea();
+		txtOut = new JTextArea();
 		txtOut.setBounds(6, 232, 543, 135);
 		getContentPane().add(txtOut);
 		
@@ -31,11 +69,25 @@ public class SecretMessagesGUI extends JFrame implements SwingConstants {
 		getContentPane().add(lblNewLabel);
 		
 		JButton btnNewButton = new JButton("Encode/Decode");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				try {
+				String message = txtIn.getText();
+				int key = Integer.parseInt( txtKey.getText() );
+				String output = encode( message, key );
+				txtOut.setText( output );
+				} catch (Exception ex) {	
+				}
+			}
+		});
 		btnNewButton.setBounds(339, 191, 156, 29);
 		getContentPane().add(btnNewButton);
 	}
 
 	public static void main(String[] args) {
+		SecretMessagesGUI theApp = new SecretMessagesGUI();
+		theApp.setSize(new java.awt.Dimension(600,400));
+		theApp.setVisible(true);
 		// TODO Auto-generated method stub
 
 	}
