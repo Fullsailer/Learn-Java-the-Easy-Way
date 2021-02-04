c4f6369c1c86b177811cf7546373565a09364085
John Flynn <40461634+Fullsailer@users.noreply.github.com>
2021-02-04 12:38:52 -0500

    Message App Update
    Improve GUI, Edited Encode/Decode button, Wrapped JTextArea input and outputs, Added bad input popup dialog, added slider for encoding and decoding key value, Exported Sharable version of the app.

--- SecretMessagesGUI/src/SecretMessagesGUI.java
+++ SecretMessagesGUI/src/SecretMessagesGUI.java
@@ -3,14 +3,21 @@ import javax.swing.SwingConstants;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.Font;
+import java.awt.Color;
+import javax.swing.JSlider;
+import javax.swing.event.ChangeListener;
+import javax.swing.event.ChangeEvent;
 
 public class SecretMessagesGUI extends JFrame implements SwingConstants {
 	private JTextArea txtIn;
 	private JTextField txtKey;
 	private JTextArea txtOut;
+	private JSlider slider;
 	public String encode( String message, int keyVal ) {
 		String output = "";
 		char key = (char) keyVal;
@@ -47,14 +54,22 @@ public class SecretMessagesGUI extends JFrame implements SwingConstants {
 		return output;
 	}
 	public SecretMessagesGUI() {
+		setTitle("John's Secret Message App");
+		getContentPane().setBackground(new Color(0, 0, 255));
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		getContentPane().setLayout(null);
 		
 		txtIn = new JTextArea();
+		txtIn.setWrapStyleWord(true);
+		txtIn.setLineWrap(true);
+		txtIn.setFont(new Font("Lucida Grande", Font.PLAIN, 18));
 		txtIn.setBounds(6, 6, 543, 182);
 		getContentPane().add(txtIn);
 		
 		txtOut = new JTextArea();
+		txtOut.setWrapStyleWord(true);
+		txtOut.setLineWrap(true);
+		txtOut.setFont(new Font("Lucida Grande", Font.PLAIN, 18));
 		txtOut.setBounds(6, 232, 543, 135);
 		getContentPane().add(txtOut);
 		
@@ -76,12 +91,37 @@ public class SecretMessagesGUI extends JFrame implements SwingConstants {
 				int key = Integer.parseInt( txtKey.getText() );
 				String output = encode( message, key );
 				txtOut.setText( output );
-				} catch (Exception ex) {	
+				} catch (Exception ex) {
+					JOptionPane.showMessageDialog(null,
+							"Please enter a whole number value for the encryption key.");
+					txtKey.requestFocus();
+					txtKey.selectAll();
 				}
 			}
 		});
-		btnNewButton.setBounds(339, 191, 156, 29);
+		btnNewButton.setBounds(338, 189, 156, 38);
 		getContentPane().add(btnNewButton);
+		
+		slider = new JSlider();
+		slider.addChangeListener(new ChangeListener() {
+			public void stateChanged(ChangeEvent e) {
+				txtKey.setText("" + slider.getValue() );
+				String message = txtIn.getText();
+				int key = slider.getValue();
+				String output = encode( message, key );
+				txtOut.setText( output );
+			}
+		});
+		slider.setValue(3);
+		slider.setMinorTickSpacing(1);
+		slider.setMajorTickSpacing(13);
+		slider.setMinimum(-26);
+		slider.setMaximum(26);
+		slider.setPaintTicks(true);
+		slider.setPaintLabels(true);
+		slider.setBackground(new Color(0, 0, 255));
+		slider.setBounds(25, 194, 190, 29);
+		getContentPane().add(slider);
 	}
 
 	public static void main(String[] args) {
