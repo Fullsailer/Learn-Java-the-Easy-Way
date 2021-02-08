2d0082273b1787b535432d7181e4cbf97ca65b43
John Flynn <40461634+Fullsailer@users.noreply.github.com>
2021-02-08 10:29:34 -0500

    Bubble Draw Build Update
    Built Method to Draw Bubble, added ArrayList for storing bubbles, added Constructor to Bubble Panel Class, Added Method to Draw on Screen, added test for class for bubble draw.

--- BubbleDraw/src/BubblePanel.java
+++ BubbleDraw/src/BubblePanel.java
@@ -1,9 +1,33 @@
+import java.util.ArrayList;
+import java.awt.Graphics;
 import java.util.Random;
 import java.awt.Color;
 import javax.swing.JPanel;
 
 public class BubblePanel extends JPanel {
 	Random rand = new Random();
+	ArrayList<Bubble> bubbleList;
+	int size = 25;
+	public BubblePanel() {
+		bubbleList = new ArrayList<Bubble>();
+		setBackground(Color.BLACK);
+		testBubbles();
+	}
+	public void paintComponent(Graphics canvas) {
+		super.paintComponent(canvas);
+		for(Bubble b : bubbleList ) {
+			b.draw(canvas);
+		}
+	}
+	public void testBubbles() {
+		for(int n = 0; n < 100; n++) {
+			int x = rand.nextInt(600);
+			int y = rand.nextInt(400);
+			int size = rand.nextInt(50);
+			bubbleList.add(new Bubble(x,y,size) );
+		}
+		repaint();
+	}
 	private class Bubble {
 		private int x;
 		private int y;
@@ -13,9 +37,13 @@ public class BubblePanel extends JPanel {
 			x = newX;
 			y = newY;
 			size = newSize;
-			color = new Color( rand.nextInt(256)),
+			color = new Color( rand.nextInt(256),
 					rand.nextInt(256),
 					rand.nextInt(256));
+		}
+		public void draw(Graphics canvas) {
+			canvas.setColor(color);
+			canvas.fillOval(x - size/2, y - size/2, size, size);
 		}
 	}
 }
